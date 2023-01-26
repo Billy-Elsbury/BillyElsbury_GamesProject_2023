bde52c59f1306e9894b950f246de197f87c23751
Billy-Elsbury <billy.elsbury@students.ittralee.ie>
2023-01-26 21:49:52 +0000

    Sync Commit

--- Assets/Scripts/BallCharacterController.cs
+++ Assets/Scripts/BallCharacterController.cs
@@ -1,10 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallCharacterController : MonoBehaviour
 {
-    public float speed = 2f;
+    enum CharacterStates { Grounded, Jumping, Falling }
+    CharacterStates ball_state = CharacterStates.Grounded;
+    
+    private Vector3 jumping_velocity;
+
+    private float start_jump_velocity = 15f;
+    private float jump_boost = 5f;
+    private float current_speed = 0f;
+
+    private float roll_speed = 5f;
+    private float gravity = 10f;
+    private bool grounded;
+
     private Rigidbody rigidBodyBall;
     private Vector3 jumping_velocity;
     float start_jump_velocity = 10;
@@ -24,24 +37,66 @@ public class BallCharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (ball_state)
+        {
+            case CharacterStates.Grounded:
+                if (shouldJump()) jump();
+                transform.position += current_speed * transform.forward * Time.deltaTime;
+                break;
+
+
+            case CharacterStates.Jumping:
+                transform.position += jumping_velocity * Time.deltaTime;
+                jumping_velocity -= gravity * Vector3.up * Time.deltaTime;
+
+                if (jumping_velocity.y < 0f)
+                {
+                    ball_state = CharacterStates.Falling;
+                }
+                break;
+
+
+            case CharacterStates.Falling:
+
+
+                /*SphereCollider[] colliding_with = (SphereCollider[])Physics.OverlapBox(transform.position, new Vector3(0.5f, 0.1f, 0.5f));
+                foreach (Collider c in colliding_with)
+                {
+                    print(c.tag);*/
+
+                    if (grounded == true)
+                    {
+                        ball_state = CharacterStates.Grounded;
+                    }
+
+                //}
+
+                break;
+
+
+        }
+
+    
+
+
         //Horizontal inputs
         if (Input.GetAxis("Horizontal") > 0)
         {
-            rigidBodyBall.AddForce(Vector3.right * speed);
+            rigidBodyBall.AddForce(Vector3.right * roll_speed);
         }
         else if (Input.GetAxis("Horizontal") < 0)
         {
-            rigidBodyBall.AddForce(-Vector3.right * speed);
+            rigidBodyBall.AddForce(-Vector3.right * roll_speed);
         }
 
         //Vertical inputs
         if (Input.GetAxis("Vertical") > 0)
         {
-            rigidBodyBall.AddForce(Vector3.forward * speed);
+            rigidBodyBall.AddForce(Vector3.forward * roll_speed);
         }
         else if (Input.GetAxis("Vertical") < 0)
         {
-            rigidBodyBall.AddForce(-Vector3.forward * speed);
+            rigidBodyBall.AddForce(-Vector3.forward * roll_speed);
         }
 
         //Jumping input
@@ -88,6 +143,7 @@ public class BallCharacterController : MonoBehaviour
         }
     }
 
+<<<<<<< Updated upstream
                 private void jump()
                 {
                     ball_state = CharacterStates.Jumping;
@@ -102,3 +158,33 @@ public class BallCharacterController : MonoBehaviour
                 }
         }
  
+=======
+        private void jump()
+        {
+            ball_state = CharacterStates.Jumping;
+
+            jumping_velocity = jump_boost * current_speed * transform.forward + start_jump_velocity * Vector3.up;
+        }
+
+        private bool shouldJump()
+        {
+            return Input.GetKey(KeyCode.Space);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.name == "Ground") return;
+            {
+                grounded = true;
+            }
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.name == "Ground") return;
+            {
+                grounded = false;
+            }
+        }
+}
+>>>>>>> Stashed changes
