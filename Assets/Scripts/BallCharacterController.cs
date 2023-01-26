8d490df94ebda641ec5006d0059d717090afe056
Billy-Elsbury <billy.elsbury@students.ittralee.ie>
2023-01-26 15:45:26 +0000

    In class
    broke everything

--- Assets/Scripts/BallCharacterController.cs
+++ Assets/Scripts/BallCharacterController.cs
@@ -6,6 +6,14 @@ public class BallCharacterController : MonoBehaviour
 {
     public float speed = 2f;
     private Rigidbody rigidBodyBall;
+    private Vector3 jumping_velocity;
+    float start_jump_velocity = 10;
+    private float jump_boost = 2;
+    private float gravity = 10;
+
+    enum CharacterStates { Grounded, Jumping, Falling }
+
+    CharacterStates ball_state = CharacterStates.Grounded;
 
     // Start is called before the first frame update
     void Start()
@@ -35,5 +43,62 @@ public class BallCharacterController : MonoBehaviour
         {
             rigidBodyBall.AddForce(-Vector3.forward * speed);
         }
+
+        //Jumping input
+
+        switch (ball_state)
+        {
+            //Grounded
+            case CharacterStates.Grounded:
+
+                print("Ball is on the ground!");
+                if (shouldJump()) jump();
+                transform.position += speed * transform.forward * Time.deltaTime;
+                break;
+            
+            //Jumping
+            case CharacterStates.Jumping:
+                transform.position += jumping_velocity * Time.deltaTime;
+                jumping_velocity -= gravity * Vector3.up * Time.deltaTime;
+
+                if (jumping_velocity.y < 0f)
+                {
+                    ball_state = CharacterStates.Falling;
+                }
+                break;
+
+            //Falling
+            case CharacterStates.Falling:
+
+
+                Collider[] colliding_with = Physics.OverlapSphere(transform.position, 0.2f);
+                foreach (Collider c in colliding_with)
+                {
+                    print(c.tag);
+
+                    if (c.tag == "Ground")
+                    {
+                        ball_state = CharacterStates.Grounded;
+                        print("Found Ground!");
+                    }
+
+                }
+
+                break;
+        }
     }
-}
+
+                private void jump()
+                {
+                    ball_state = CharacterStates.Jumping;
+                    print("Ball is Jumping!");
+
+                    jumping_velocity = jump_boost * speed * transform.forward + start_jump_velocity * Vector3.up;
+                }
+
+                private bool shouldJump()
+                {
+                    return Input.GetKey(KeyCode.Space);
+                }
+        }
+ 
